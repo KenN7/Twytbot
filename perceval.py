f35b95d322485f283d1ffb53129ff335096e752b
KenN7 <ken@kenh.fr>
2013-12-13 11:28:54 +0100

    better logging both in console and file

--- perceval.py
+++ perceval.py
@@ -6,7 +6,11 @@ import time
 import settings
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='perceval.log')
+logging.basicConfig(level=logging.INFO, filename='perceval.log', format='%(asctime)s:%(name)s:%(levelname)-8s %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
+logging.getLogger().addHandler(console)
 
 perceval = Twytbot.twytbot(settings.C_KEY, settings.C_SECRET, settings.ACCESS_TOK, settings.SECRET_TOK, settings.NAME)
 
